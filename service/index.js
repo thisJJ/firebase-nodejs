b2da7d9f69448698f4416cd95dc2b9280bbec382
Thinnakrit <thinnakrit.idesign@gmail.com>
2017-03-04 23:26:59 +0700

    update new options

--- service/index.js
+++ service/index.js
@@ -5,6 +5,7 @@ import updateData from '../module/update';
 import deleteData from '../module/delete';
 import authFacebook from '../module/authFacebook';
 import * as authUser from '../module/authUser';
+import authFacebook from '../module/authFacebook';
 
 export default {
     firebaseConfig,
@@ -12,5 +13,6 @@ export default {
     selectData,
     updateData,
     deleteData,
-    authUser
+    authUser,
+    authFacebook
 };
